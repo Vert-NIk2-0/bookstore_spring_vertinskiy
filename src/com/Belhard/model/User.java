44aa2cf0de14a925f1df912a92f24b9c6b5b14bb
Nikita Vertinskiy <2906026@mail.ru>
2024-10-28 14:18:14 +0300

    Create User.java, Gender.java

---
+++ src/com/Belhard/model/User.java
@@ -0,0 +1,98 @@
+package com.Belhard.model;
+
+import com.Belhard.enums.Gender;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class User {
+    private Long id;
+    private String firstName;
+    private String secondName;
+    private String email;
+    private Date dateOfBirth;
+    private String phoneNumber;
+    private Gender gender;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(firstName, user.firstName) && Objects.equals(secondName, user.secondName) && Objects.equals(email, user.email) && Objects.equals(dateOfBirth, user.dateOfBirth) && Objects.equals(phoneNumber, user.phoneNumber) && gender == user.gender;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, secondName, email, dateOfBirth, phoneNumber, gender);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", first_name='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", email='" + email + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", phonenumber='" + phoneNumber + '\'' +
+                ", gender=" + gender +
+                '}';
+    }
+}
