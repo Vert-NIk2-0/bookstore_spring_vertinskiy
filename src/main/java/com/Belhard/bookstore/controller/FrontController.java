29d6f582802b7f0dd919bf5de15e0f1d148cc8cf
Nikita Vertinskiy <2906026@mail.ru>
2024-11-13 21:19:50 +0300

    Create FrontController.java

---
+++ src/main/java/com/Belhard/bookstore/controller/FrontController.java
@@ -0,0 +1,32 @@
+package com.belhard.bookstore.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
+
+import java.io.IOException;
+
+@Log4j2
+@WebServlet("/controller")
+public class FrontController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            String commandParam = req.getParameter("command");
+            Command command = CommandFactory.INSTANCE.get(commandParam);
+            String page = command.process(req);
+            req.getRequestDispatcher(page).forward(req, resp);
+        } catch (Exception e) {
+            log.error(e);
+            Command command = CommandFactory.INSTANCE.get("error");
+            String page = command.process(req);
+            req.getRequestDispatcher(page).forward(req, resp);
+        }
+    }
+}
+
+
