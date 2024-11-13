3fd22d4b652ecbe2f29e2ccab74bd0e0d614b3b6
Nikita Vertinskiy <2906026@mail.ru>
2024-11-13 21:18:38 +0300

    Create BookCommand.java

---
+++ src/main/java/com/Belhard/bookstore/controller/impl/BookCommand.java
@@ -0,0 +1,32 @@
+package com.belhard.bookstore.controller.impl;
+
+import com.belhard.bookstore.controller.Command;
+import com.belhard.bookstore.dao.BookDao;
+import com.belhard.bookstore.dao.entity.Book;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RequiredArgsConstructor
+public class BookCommand implements Command {
+    private final BookDao bookDao;
+
+    public String process(HttpServletRequest req){
+        long id = getId(req);
+        Book book = bookDao.getById(id);
+        req.setAttribute("book", book);
+        req.setAttribute("date", LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy - HH:mm:ss")));
+        return "jsp/book.jsp";
+
+    }
+
+    private long getId(HttpServletRequest req) {
+        String reqId = req.getParameter("id");
+        return Long.parseLong(reqId);
+    }
+}
+
+
