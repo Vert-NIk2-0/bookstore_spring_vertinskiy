7b33953a0f9accce0d395cd8238e9286da2ee22c
Nikita Vertinskiy <2906026@mail.ru>
2024-11-23 18:16:38 +0300

    Create new beans:
    JdbcTemplate
    DataSource

--- src/main/java/com/Belhard/bookstore/AppConfig.java
+++ src/main/java/com/Belhard/bookstore/AppConfig.java
@@ -1,11 +1,49 @@
 package com.belhard.bookstore;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.util.Date;
 
 @Configuration
 @ComponentScan
-@PropertySource("classpath:application.properties")
+@PropertySource("classpath:/application.properties")
 public class AppConfig {
+
+    @Value("${db.driver}")
+    private String driver;
+
+    @Value("${db.url}")
+    private String url;
+
+    @Value("${db.username}")
+    private String username;
+
+    @Value("${db.password}")
+    private String password;
+
+    @Value("${db.poolSize}")
+    private int poolSize;
+
+    @Bean
+    public JdbcTemplate template (DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource source = new HikariDataSource();
+        source.setDriverClassName(driver);
+        source.setJdbcUrl(url);
+        source.setUsername(username);
+        source.setPassword(password);
+        source.setMaximumPoolSize(poolSize);
+        return source;
+    }
 }
